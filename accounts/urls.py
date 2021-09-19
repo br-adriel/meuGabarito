6b416726ae7a11c12d6a3d45ed90aec5b27a663c
Adriel <adriel.fariasan@gmail.com>
2021-09-18 22:56:39 -0300

    Implementacao do cadastro de usuarios #1

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-
+from .views import criar_conta_view
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register', criar_conta_view, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def criar_conta_view(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            # login do usuario criado
+            username = form.cleaned_data.get('username')
+            senha = form.cleaned_data.get('password1')
+            usuario = authenticate(request, username=username, password=senha)
+
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('login')
+    return render(request, 'registration/criar_conta.html', {'form' : form})
