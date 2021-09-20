87b24389ad9d1deb10d76dced4b1f7d535a0be07
Adriel <adriel.fariasan@gmail.com>
2021-09-20 20:01:03 -0300

    Criado form de novo gabarito #5

---
+++ gabaritos/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Gabarito
+
+
+class CriarGabaritoForm(forms.ModelForm):
+    class Meta:
+        model = Gabarito
+        fields = ('nome', 'tamanho', 'indice')
+        help_texts = {
+            'tamanho' : 'Número de questões do seu gabarito.',
+            'indice' : 'O número da primeira questão.',
+        }
